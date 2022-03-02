f6f508619f11ba67dbdcc7615de0ae1ca07ce10d
Ismail Mohammed Tayeb <tayebismail@yahoo.com>
2022-03-02 12:29:56 +0200

    Add project files.

---
+++ TestingCoreAPI/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace TestingCoreAPI.Controllers
+{
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("api/Test/Get")]
+
+        public IActionResult Get() {
+            return Ok(new Random().Next(0, 200));
+        }
+
+        [HttpGet]
+        [Route("api/Test/GetBiggerRandom")]
+
+        public int GetBiggerRandom()
+        {
+            return new Random().Next(500, 9999);
+        }
+
+    }
+}
